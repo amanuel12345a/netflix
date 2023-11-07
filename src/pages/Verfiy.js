35357e762c5d6e45704b166a9d0445421745d220
amanuel12345a <am@nuelsew@gegn@gmail.com>
2023-11-07 11:39:03 -0600

    edit

--- src/pages/Verfiy.js
+++ src/pages/Verfiy.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom';
+
 
 function Verfiy() {
+    // axios.get('https://aman-netflix.onrender.com')
+    const params = useParams()
   return (
     <div>
+        {params}
       verified
     </div>
   )
